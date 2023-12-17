6c85243f100fa9bc38a27fcad37da20bf4c27625
Harsh Gupta <142380137+Harsh70509@users.noreply.github.com>
2023-12-17 22:00:17 +0530

    Create onclick.js

---
+++ onclick.js
@@ -0,0 +1,11 @@
+Using OnClick
+
+<button type="button" onclick="document.write(5 + 6)">Try it</button>
+
+<script>
+
+window.alert(5 + 6); // only alert also works </script>
+
+On buttonclick call alert();
+
+<button onclick="window.print()">Print this page</button>
